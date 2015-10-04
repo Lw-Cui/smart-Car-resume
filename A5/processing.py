918b4738d5938c23a2dd93125d54f1fb9531cb60
Lw-Cui <645739460@QQ.com>
2015-10-04 21:36:43 +0800

    perfect image processing

--- A5/processing.py
+++ A5/processing.py
@@ -2,22 +2,25 @@
 # -*- coding: utf8 -*-
 import Image
 import pylab
+import numpy
 
-im = pylab.array(Image.open("lBend.bmp").convert('L'))
-'''
-for i in range(40):
-	y_axis = im[i]
-	x_axis = list(range(im[0].size))
-	pylab.plot(x_axis, y_axis, 'or-')
-	pylab.show()
-'''
+def display(array):
+	for i in range(array.shape[0]):
+		y_axis = array[i]
+		x_axis = list(range(array[0].size))
+		pylab.plot(x_axis, y_axis, 'or-')
+		pylab.show()
 
-for i in range(im.shape[0]):
-	for j in range(im[i].size):
-		if im[i][j] < 255 / 2:
-			im[i][j] = 0
-		else:
-			im[i][j] = 255
+def extreme(value):
+	return (0 if value < 255 / 2 else 255)
+
+def square(value):
+	return 255.0 * (value / 255.0)**2
+
+
+im = pylab.array(Image.open("sBend.bmp").convert('L'))
+im = numpy.vectorize(square)(im)
+#display(im)
 
 im = Image.fromarray(im)
 im.show()
